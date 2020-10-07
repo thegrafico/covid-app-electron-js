8d4f4cd959771ec355f34cb6bd2b93625e8c4790
Raul Pichardo <raul022107@gmail.com>
2020-10-06 22:28:39 -0700

    Added delete btn and date format

--- public/js/helper.js
+++ public/js/helper.js
@@ -3,13 +3,16 @@ const table = "#state_county_table";
 const stateInput = "#stateInput";
 const countyInput = "#countyInput";
 
-let state_county_arr = [];
-
+let DAYS_BEFORE = 21;
+let state_county = [];
+let temp = {};
+let INDEX = 0;
 $(document).ready(function() {
 
     $(addBtn).click(function() {
 
-        let table_row = `<tr> <td> XXX </td> <td> YYY </td> <td><i style="color: red;" class="fa fa-trash" aria-hidden="true"></i> </td> </tr> `;
+        //let table_row = `<tr> <td> XXX </td> <td> YYY </td> <td><button id="btnDelete">Delete</button></td> </tr> `;
+        let table_row = `<tr id='ZZZ'> <td> XXX </td> <td> YYY </td> <td> <i style="color: red;" class="fa fa-trash btnTrash" aria-hidden="true"> </i> </td> </tr> `;
 
         let state = $(stateInput).val();
         let county = $(countyInput).val();
@@ -18,23 +21,80 @@ $(document).ready(function() {
             Verify is state and county is not empty.  to notify the user use this functon -> alert("Message")
         */
 
-        let isDuplicate = state_county_arr.some((element) => element == state + county);
+        let isDuplicate = false;
+        state_county.forEach(dict => {
+            if (Object.keys(dict).length > 0) {
+                Object.keys(dict).forEach(inner => {
+                    console.log(dict[inner], " == ", state + county, (state + county) == dict[inner]);
+                    if ((state + county) == dict[inner]) {
+                        isDuplicate = true;
+                    }
+                });
+            }
+        });
 
-        console.log(isDuplicate);
-
-        // dont add it if the state and county already exits
         if (isDuplicate) return;
 
-        state_county_arr.push(state + county);
+        // state_county_arr.push(state + county);
         table_row = table_row.replace("XXX", state);
         table_row = table_row.replace("YYY", county);
+        table_row = table_row.replace("ZZZ", INDEX);
+        let key = INDEX.toString();
+        temp[key] = state + county;
+        state_county.push(temp);
+        INDEX++;
+
 
         // $('#state_county_table > tbody:last-child').append('<tr>One</tr><tr>Two</tr>');
         $('#state_county_table > tbody').last().after(table_row);
-        console.log("DONE");
+        temp = {};
+        // console.log("INDEX: ", INDEX);
+        // console.log(state_county);
     });
 
+    $('body').on('click', 'i.btnTrash', function() {
+
+        let id = $(this).parent().parent().attr('id').toString();
+        console.log("ID TO DELETE: ", id);
+        state_county = state_county.filter(dict => {
+
+            let condition = (id in dict);
+
+            if (!condition) {
+                return true;
+            }
+            return false;
+        });
+
+        console.log(state_county)
+        $(this).parent().parent().remove();
+
+    });
+
+    // TIME PERIOD
+    $("#time-period-title").text(DAYS_BEFORE);
+    $("#date_1").text(get_days_before(null));
+    $("#date_2").text(get_days_before(DAYS_BEFORE));
+
 });
+
+
+
+function get_days_before(time) {
+
+    let today = new Date();
+
+    if (time != null) {
+        today.setDate(today.getDate() - time);
+    }
+    return `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+}
+
+// $('.list').on('click', 'span', (e) => {
+//     $(e.target).parent().remove();
+//   });
+
+
 
 
 /* TODO: for the date in the box 
